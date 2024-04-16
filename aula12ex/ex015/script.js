56324180b933879038a9471d018f57898a82cdf4
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-16 02:58:28 -0300

    ok

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,11 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    varfano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.lenght == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else [
+        window.alert('tudo ok')
+    ]
+}
