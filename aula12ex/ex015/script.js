6cb1e6a60d65223e3a4b930975eca7ac839a0ed2
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-16 03:00:33 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,7 +1,7 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    varfano = document.getElementById('txtano')
+    var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     if (fano.value.lenght == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente')
