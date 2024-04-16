e286bb957f2e058453884a59057eed9d7e664f58
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-16 03:21:30 -0300

    ok

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,10 +9,31 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src',)
+            } else if (idade < 21) {
+
+            } else if (idade > 50) {
+
+            } else {
+
+            }
+
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+
+            } else if (idade < 21) {
+
+            } else if (idade > 50) {
+
+            } else {
+                
+            }
         }
         res.computedStyleMap.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
