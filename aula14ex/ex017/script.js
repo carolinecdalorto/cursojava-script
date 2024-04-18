0581c133159e8f79959f53e6526769f54e26a16a
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-18 02:31:02 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -8,6 +8,8 @@ function tabuada() {
         var c = 1
         while (c <= 10) {
             var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
         }
     }
 }
