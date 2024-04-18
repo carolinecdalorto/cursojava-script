c941448a7993c1c9288727857e97b32cbb09243a
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-17 22:01:11 -0300

    ok

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,11 +1,18 @@
 function contar() {
-    var ini = document.getElementById('txt1')
+    var ini = document.getElementById('txti')
     var fim = document.getElementById('txtf')
     var passo = document.getElementById('txtp')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.ini.lenght == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        for (var c = i; c <= f; c +=p) {
+            res.innerHTML += `${c}`
+        }
     }
 }
