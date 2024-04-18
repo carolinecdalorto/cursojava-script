df06c88f01417c0348a31d30fcb8967337c62b50
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-18 02:18:28 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,7 +1,7 @@
 function tabuada() {
     var num = document.getElementById('txtn')
     var tab = document.getElementById('seltab')
-    if (num.value.lenght == 0) {
+    if (num.value.length  0) {
         window.alert('Por favor, digite um número!')
     } else {
         var n = Number(num.value)
