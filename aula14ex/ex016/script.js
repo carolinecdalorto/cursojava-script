0a2ef4431706f7fb156ba82d4cd0f949ea2d1a76
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-18 02:05:35 -0300

    ok

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,28 +1,9 @@
-function contar() {
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0) {
-        window.alert('[ERRO] Faltam dados!')
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.lenght == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
-        res.innerHTML = 'Contando: '
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        if (p <= 0) {
-            window.alert('Passo inválido!')
-            p = 1
-        }
-        if (i < f) {
-            for(var c = i; c <= f; c +=p) {
-                res.innerHTML += ` ${c} \u{1F449}`
-            } 
-        } else {
-            for(var c = i; c>= f; c -= p) { 
-                res.innerHTML += ` ${c} \u{1F449}`
-            }
-        }
-    res.innerHTML += `\u{1F3C1}`
+        window.alert('Por favor, digite um número!')
     }
 }
