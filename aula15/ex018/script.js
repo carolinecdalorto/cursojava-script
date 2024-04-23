44076f795238ece7830fac3301273d1d9e421cd9
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-23 20:06:29 -0300

    Update script.js

--- aula15/ex018/script.js
+++ aula15/ex018/script.js
@@ -1,28 +1,7 @@
-function contar() {
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('selecte#flista')
+let res = document.querySelector('div#res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0) {
-        window.alert('[ERRO] Faltam dados!')
-    } else {
-        res.innerHTML = 'Contando: '
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        if (p <= 0) {
-            window.alert('Passo inválido!')
-            p = 1
-        }
-        if (i < f) {
-            for(var c = i; c <= f; c +=p) {
-                res.innerHTML += ` ${c} \u{1F449}`
-            } 
-        } else {
-            for(var c = i; c>= f; c -= p) { 
-                res.innerHTML += ` ${c} \u{1F449}`
-            }
-        }
-    res.innerHTML += `\u{1F3C1}`
-    }
+function adicionar() {
+    
 }
