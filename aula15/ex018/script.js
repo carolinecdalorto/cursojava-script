b9b6cf6cfb73bc9f4ab29c754fee3b23f73b02b0
carolinecdalorto <158402770+carolinecdalorto@users.noreply.github.com>
2024-04-23 20:23:30 -0300

    Update script.js

--- aula15/ex018/script.js
+++ aula15/ex018/script.js
@@ -1,7 +1,24 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('selecte#flista')
 let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    }
+}
 
 function adicionar() {
-    
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo ok!')
+    } else {
+        window.alert('valor inválido ou já encontrado na lista.')
+    }
 }
